8ce7ef120194bf74a08b0d3fe638de97d4fb2d53
ACERY1 <y841034081@gmail.com>
2019-03-14 08:13:14 +0800

    feat: 初始化配置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import './App.less';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <Button type="primary">Button</Button>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
