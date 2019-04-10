8ef4cfc0971546c6b70ac27921bbd9bf51ebf7a6
ACERY1 <y841034081@gmail.com>
2019-04-10 11:47:02 +0800

    feat:完成编辑项目功能 & 解决内存溢出问题

--- src/renderer/views/Project/index.js
+++ src/renderer/views/Project/index.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import "./index.less";
 import * as EVENTS from "../../../shared/events";
 import { withRouter } from "react-router";
-import { message } from "antd";
+import { message, Row, Col } from "antd";
 
+import logo from "@assets/logo.png";
 const { ipcRenderer } = window.electron;
 
 class Project extends Component {
@@ -21,13 +22,12 @@ class Project extends Component {
     };
   }
 
-  
   componentDidMount() {
     if (!this.props.match.params.id) {
       return message.error("无效的appid");
     }
     ipcRenderer.on(EVENTS.GET_PROJECT_BY_ID, (evt, projectInfo) => {
-      const data = Object.assign(this.state.projectInfo, projectInfo)
+      const data = Object.assign(this.state.projectInfo, projectInfo);
       this.setState({
         projectInfo: data
       });
@@ -36,10 +36,9 @@ class Project extends Component {
     // 初次进入页面请求一次
     ipcRenderer.send(EVENTS.GET_PROJECT_BY_ID, this.props.match.params.id);
   }
-  
 
   componentWillUnmount() {
-    ipcRenderer.removeListener(EVENTS.GET_PROJECT_BY_ID, () => {})
+    ipcRenderer.removeListener(EVENTS.GET_PROJECT_BY_ID, () => {});
   }
 
   render() {
@@ -53,9 +52,21 @@ class Project extends Component {
     } = this.state.projectInfo;
 
     return (
-      <div className="project">
-        {name}
-        {desc}
+      <div className="project p20">
+        <Row type="flex" align="top">
+          <Col span={4}>
+            <img src={logo} alt="logo" className="project-logo bdr1_ra4" />
+          </Col>
+          <Col span={10}>
+            <p className="t3 w2 c1 mb5 mt10">{name}</p>
+            <p className="t4 w3 c3 mb20">{desc}</p>
+            <p className="t5 w3 c3">appid: {appid}</p>
+          </Col>
+          <Col span={10}>
+            <p className="mt30 mb10 t5 w2 c2">Last Modify Time: {updateTime}</p>
+            <p className="t5 w2 c2">path: {path}</p>
+          </Col>
+        </Row>
       </div>
     );
   }
